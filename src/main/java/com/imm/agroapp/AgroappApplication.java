3a8171fff6145e3106e1366d43dc2013e4717c69
Ilyass Mahdiyan <ilyassmah00@gmail.com>
2023-04-09 16:46:52 +0200

    Remove imports

--- src/main/java/com/imm/agroapp/AgroappApplication.java
+++ src/main/java/com/imm/agroapp/AgroappApplication.java
@@ -2,9 +2,6 @@ package com.imm.agroapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
 public class AgroappApplication {
